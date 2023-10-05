c14ab120d4e035c85c09b4493deee6ce3d81bee4
marcelo <33940878+marcelo-pro@users.noreply.github.com>
2023-10-04 22:59:45 -0300

    Create criarconta.java

---
+++ criarconta.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro:");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro:");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        System.out.println("Contagem:");
+        for (int i = 0; i <= contagem; i++) {
+            System.out.println(parametroUm + i);
+        }
+    }
+}
+
